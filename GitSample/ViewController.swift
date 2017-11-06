347aee5aa2788d8df7784ac628336e43d66dc6e1
ekramramu <ekramramu@gmail.com>
2017-11-06 17:07:13 +0600

    view controller changed

--- GitSample/ViewController.swift
+++ GitSample/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       print("changes the view controller ")
     }
 
     override func didReceiveMemoryWarning() {
